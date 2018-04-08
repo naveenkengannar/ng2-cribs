6231f4db1b30b61b69eb335fbb41c89d0b339c11
Naveen Kengannar Manjappa <NManjappa@manh.com>
2018-04-08 12:23:06 +0530

    Moved to reusable service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
+import { CribsService } from './cribs.service';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CribListingComponent
+    CribListingComponent,
+    CribCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [CribsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { cribs } from './../data/cribs';
+import { CribsService } from '../cribs.service';
 
 @Component({
   selector: 'app-crib-listing',
@@ -8,11 +8,16 @@ import { cribs } from './../data/cribs';
 })
 export class CribListingComponent implements OnInit {
 
-  cribs: Array<any> = cribs;
-  constructor() { }
+  cribs: Array<any>;
+  error: string;
+  constructor(private cribsService: CribsService) { }
 
   ngOnInit() {
-    
+    this.cribsService.getAllCribs()
+    .subscribe(
+      data => this.cribs = data,
+      error => console.log(error.statusText)      
+    );
   }
 
 }
